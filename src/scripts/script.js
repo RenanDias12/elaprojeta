68035e4c6b24c520a4b9d1c7f3231fc02ffb18d1
Renan Faria <renandias@gec.inatel.br>
2024-11-20 16:23:57 -0300

    Finishing about modal and add social media icons and others sections

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -27,23 +27,4 @@ document.addEventListener('DOMContentLoaded', (event) => {
             }
         });
     });
-
-    //modal
-    const modal = document.getElementById("myModal");
-    const btn = document.getElementById("cta-button");
-    const span = document.getElementsByClassName("close")[0];
-
-    btn.onclick = () => {
-        modal.style.display = "block";
-    }
-
-    span.onclick = () => {
-        modal.style.display = "none";
-    }
-
-    window.onclick = (event) => {
-        if (event.target == modal) {
-            modal.style.display = "none";
-        }
-    }
 });
